6ce32677eca10f2dd6bd6512055b17679b39b17a
Andrej Adamcik <ani.mack@gmail.com>
2016-11-11 21:54:26 +0000

    api updates

--- extension.js
+++ extension.js
@@ -13,12 +13,15 @@ define(function(require, exports, module) {
 		name: 'typescript-compiler'
 	}, {
 		init: function() {
-			var self = this;
-			EditorSession.on('save', function(e) {
-				if (self._exts.indexOf(e.storage.extension) !== -1) {
-					Extension.compile(e.storage.workspaceId, e.storage.path, e.session.data.$worker, e.session.data.getValue());
-				}
-			});
+			EditorSession.on('save', this.onSave);
+		},
+		destroy: function() {
+			EditorSession.off('save', this.onSave);
+		},
+		onSave: function(e) {
+			if (Extension._exts.indexOf(e.storage.extension) !== -1) {
+				Extension.compile(e.storage.workspaceId, e.storage.path, e.session.data.$worker, e.session.data.getValue());
+			}
 		},
 		_exts: ['ts'],
 		_underscores: false,
