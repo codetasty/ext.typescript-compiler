83fe68b3d8d98e522b3ee109112f5db942f1c610
Andrej Adamcik <ani.mack@gmail.com>
2017-06-17 13:48:31 +0000

    update compiler

--- README.md
+++ README.md
@@ -1,21 +1,62 @@
-# typescript-compiler
+# TypeScript compiler
 
 typescript-compiler is an extension for the code editor CodeTasty that adds automatic compilation of TypeScript files upon saving.
 
+## Configuration
 
-### Compile Options
+### 1, Configuration file (recommended)
 
-TypeScript compile options can be set in the first line of the edited file:
+Create or edit **codetasty.json** file in the workspace root.
 
-    // out: ../js/module.js
+```
+{
+    "extension": {
+        "typescript-compiler": {
+            "files": {
+                "source": [
+                    "js/main.coffee",
+                    "js/app.coffee"
+                ],
+                "output": "bundle.js"
+            }
+        }
+    }
+}
+```
 
-out: compiled file destination
+#### files
+Type: `Array|Object`
 
-    // out: ., module.js, ../module.js
-    // . - same path with js extension
-    // something.ts - will compile this file instead
-    
-All other compile options should be set in /tsconfig.json file in the root of a workspace.
+Can be also array to compile multiple bundles.
+
+#### files.source
+Type: `Array`, Required
+
+List of files to compile, these file will be watched and auto compiled on save.
+
+#### files.output
+Type: `String`, Required
+
+Destination where the compiled output is saved.
+
+
+### 2, Inline comment (deprecated)
+
+Compile options can be set in the first line of the edited file, separated by comma.
+
+    # out: ../js/main.js
+
+#### out
+Type: `String`, Required
+
+Sets output file.
+
+    # out: ., app.js, ../main.js
+    # . - same path with js extension
+
+
+## TypeScript Configuration
+All other compile options should be set in **tsconfig.json** file in the workspace root.
 
     {
         "compilerOptions": {
--- extension.js
+++ extension.js
@@ -1,115 +1,84 @@
 define(function(require, exports, module) {
 	var ExtensionManager = require('core/extensionManager');
 	
-	var Socket = require('core/socket');
-	var Workspace = require('core/workspace');
-	var Notification = require('core/notification');
-	var Fn = require('core/fn');
+	var Utils = require('core/utils');
 	var FileManager = require('core/fileManager');
 	
 	var EditorSession = require('modules/editor/ext/session');
 	var EditorEditors = require('modules/editor/ext/editors');
+	var EditorCompiler = require('modules/editor/ext/compiler');
 	
 	var Extension = ExtensionManager.register({
 		name: 'typescript-compiler'
 	}, {
+		watcher: null,
+		compilerName: 'TypeScript',
 		init: function() {
-			EditorEditors.on('save', this.onSave);
+			this.watcher = EditorCompiler.addWatcher(this.name, {
+				property: 'source',
+				extensions: ['ts'],
+				outputExtension: 'js',
+				comments: true,
+				watch: this.onWatch.bind(this),
+			});
 		},
 		destroy: function() {
-			EditorEditors.off('save', this.onSave);
+			this.watcher = null;
+			EditorCompiler.removeWatcher(this.name);
 		},
-		onSave: function(session, value) {
-			if (Extension._exts.indexOf(session.storage.extension) !== -1) {
-				Extension.compile(session.storage.workspaceId, session.storage.path, session.data.$worker, value);
-			}
-		},
-		_exts: ['ts'],
-		_underscores: false,
-		importWorkspace: null,
-		importPath: '',
-		compile: function(workspaceId, path, $worker, doc) {
-			if (!$worker) {
-				return false;
-			}
-			
-			var self = this;
-			var options = FileManager.getFileOptions(doc);
-			
-			if (!options.out) {
-				return false;
-			}
-			
-			var destination = FileManager.parsePath(path, options.out, [this._exts.join('|'), 'js']);
-			
-			if (!destination) {
-				return false;
-			}
-			
-			if (destination.match(/\.ts/)) {
-				FileManager.getCache(workspaceId, destination, function(data, err) {
-					if (err) {
-						return Notification.open({
-							type: 'error',
-							title: 'TypeScript compilation failed',
-							description: err.message,
-							autoClose: true
-						});
-					}
-					
-					Extension.compile(workspaceId, destination, data);
-				});
-				
-				return false;
-			}
-			
-			this.importWorkspace = workspaceId;
-			this.importPath = path;
-			
-			var $notification = Notification.open({
-				type: 'default',
-				title: 'TypeScript compilation',
-				description: 'Compiling <strong>' + path + '</strong>',
-			});
-			
-			//get configuration
+		onWatch: function(workspaceId, obj, session, value) {
 			FileManager.getCache(workspaceId, '/tsconfig.json', function(data, err) {
 				var config = {};
-				if (data) {
-					try { 
-						config = JSON.parse(data);
-					} catch (e) {
-						
+				try { 
+					config = data ? JSON.parse(data) : config;
+				} catch (e) {}
+			
+				EditorCompiler.addCompiler(this.watcher, this.compilerName, workspaceId, obj, function(compiler) {
+					if (!session.data.$worker) {
+						compiler.destroy(new Error('Worker not loaded'));
+						return EditorCompiler.removeCompiler(compiler);
+					}
+					
+					compiler.files = compiler.source.map(function(path) {
+						return {
+							path: path,
+							file: null
+						};
+					});
+					
+					compiler.worker = session.data.$worker;
+					compiler.config = config;
+					compiler.compiled = 0;
+					
+					compiler.file = this.onFile.bind(this);
+				}.bind(this));
+			}.bind(this));
+		},
+		onFile: function(compiler, path, file) {
+			compiler.worker.call('compile', [file, compiler.config], function(data) {
+				if (data.errors.length) {
+					compiler.destroy(new Error(
+						__('%s on <strong>%s:%s</strong> in file <strong>%s</strong>.', data.errors[0].text, data.errors[0].row+1, data.errors[0].column+1, Utils.path.humanize(path))
+					));
+					
+					return EditorCompiler.removeCompiler(compiler);
+				}
+				
+				for (var i = 0; i < compiler.files.length; i++) {
+					if (compiler.files[i].path === path) {
+						compiler.files[i].file = data.output;
+						compiler.compiled++;
+						break;
 					}
 				}
 				
-				$worker.call('compile', [doc, config], function(data) {
-					if (data.errors.length) {
-						$notification.trigger('close');
-						
-						return Notification.open({
-							type: 'error',
-							title: 'TypeScript compilation failed',
-							description: data.errors.map(function(error) {
-								return error.text + ' <strong>(' + (error.row+1) + ':' + (error.column+1) + ')</strong>';
-							}).join('<br>'),
-							autoClose: true
-						});
-					}
-					
-					FileManager.save({
-						id: workspaceId,
-						path: destination,
-						data: function() {
-							$notification.trigger('close');
-						},
-						error: function() {
-							$notification.trigger('close');
-						}
-					}, data.output);
-				});
+				if (compiler.compiled === compiler.files.length) {
+					EditorCompiler.saveOutput(compiler, compiler.files.map(function(file) {
+						return file.file;
+					}).join("\n"));
+				}
 			});
-		}
+		},
 	});
 
 	module.exports = Extension;
